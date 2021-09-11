79f18552e447045a7e065a1245581ef1635f00d6
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-09-11 23:33:08 +0800

    优化存储;

---
+++ app/src/main/java/cn/misection/autoreport/ui/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package cn.misection.autoreport.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.misection.autoreport.R;
+
+/**
+ * @author Administrator
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
