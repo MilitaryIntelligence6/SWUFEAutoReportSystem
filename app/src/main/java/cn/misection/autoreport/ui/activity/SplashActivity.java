92cf4ba6a127d4347491f348c97731b0f4873eef
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-09-11 23:44:44 +0800

    加入 splash 页面;

--- app/src/main/java/cn/misection/autoreport/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/misection/autoreport/ui/activity/SplashActivity.java
@@ -2,7 +2,10 @@ package cn.misection.autoreport.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import cn.misection.autoreport.R;
 
@@ -11,9 +14,27 @@ import cn.misection.autoreport.R;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASH_DISPLAY_TIME = 500;
+
+    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        init();
+        splash();
+    }
+
+    private void init() {
+        mHandler = new Handler(Looper.myLooper());
+    }
+
+    private void splash() {
+        mHandler.postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            SplashActivity.this.finish();
+        }, SPLASH_DISPLAY_TIME);
     }
 }
