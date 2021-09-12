841ee55af5446802664f3b12bc1dd81b9ddfe147
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-09-12 16:49:02 +0800

    优化 ui;

---
+++ app/src/main/java/cn/misection/autoreport/common/receiver/AdminReceiver.java
@@ -0,0 +1,14 @@
+package cn.misection.autoreport.common.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * @author Military Intelligence 6 root
+ * @version 1.0.0
+ * @ClassName AdminReceiver
+ * @Description TODO
+ * @CreateTime 2021年09月12日 16:37:00
+ */
+public class AdminReceiver extends DeviceAdminReceiver {
+
+}
